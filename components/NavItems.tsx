3691a6c176ebd5655fb779209060fd36b1b717ee
idris <kulubiidris@gmail.com>
2024-07-04 11:03:30 +0300

    added navbar and the landing page

---
+++ components/NavItems.tsx
@@ -0,0 +1,14 @@
+
+'use client'
+
+import { useState } from "react"
+
+const Navitems =() =>{
+
+
+    const [activeIndex,setActiveIndex] = useState < 
+    null|Number
+    >(null)
+return<div className="flex gap-4 h-full"></div>
+}
+export default Navitems
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const    PRODUCT_CATEGORIES =[
+    {
+        label: 'UI KITS',
+        value:"UI_KITS" as const,
+        feature:[
+            {
+                name:'Editor picks',
+                href: '',
+                imageSrc:'/nav/ui-kits/mixed.jpg',
+            }
+        ]
+    }
+]
