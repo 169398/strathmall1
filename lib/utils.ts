2191d721cfc45cb1b2ccdc0963e0b9966a65572c
idris <kulubiidris@gmail.com>
2024-07-04 20:50:38 +0300

    added a the  cart

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(price: number | string,
+
+  options: {
+    currency?:"USD"| "EUR" | "GBP" | "JPY" |"KSH"| "NGN" | "RWF" | "TZS" | "UGX" | "ZAR" | "ZMW" ,
+    notation?: Intl.NumberFormatOptions["notation"]
+  } = {}
+) {
+const{currency = "KSH", notation = "compact"} = options
+
+const numericPrice = typeof price === "string" ? parseFloat(price) : price
+
+return new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency,
+  notation,
+  maximumFractionDigits: 0
+}).format(numericPrice)
+}
+
+
