23debc41fc284ffaa5a58eacd7f6111aeb7ffdbf
idris <kulubiidris@gmail.com>
2024-07-04 15:37:23 +0300

    navbar components added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,6 +32,8 @@ const Navbar = () => {
 
                 <NavItems/>
               </div>
+
+              
             </div>
           </div>
         </MaxWidthWrapper>
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,44 @@
-export const    PRODUCT_CATEGORIES =[
-    {
-        label: 'UI KITS',
-        value:"UI_KITS" as const,
-        feature:[
-            {
-                name:'Editor picks',
-                href: '',
-                imageSrc:'/nav/ui-kits/mixed.jpg',
-            }
-        ]
-    }
-]
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI KITS",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "/",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "/",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "Best sellers",
+        href: "/",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favourite Shop picks",
+        href: "/",
+        imageSrc: "/nav/icons/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "/",
+        imageSrc: "/nav/icons/mixed.jpg",
+      },
+      {
+        name: "Best selling products",
+        href: "/",
+        imageSrc: "/nav/icons/mixed.jpg",
+      },
+    ],
+  },
+];
