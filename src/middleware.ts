eb9fb9245a4d18fcc9c63d3f075f93b2eac42a97
idris <kulubiidris@gmail.com>
2024-07-10 20:12:03 +0300

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getServerSideUser } from './lib/payload-utils'
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSideUser } from "./lib/payload-utils";
 
 export async function middleware(req: NextRequest) {
-  const { nextUrl, cookies } = req
-  const { user } = await getServerSideUser(cookies)
+  const { nextUrl, cookies } = req;
 
-  if (
-    user &&
-    ['/sign-in', '/sign-up'].includes(nextUrl.pathname)
-  ) {
-    return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/`
-    )
+  try {
+    const { user } = await getServerSideUser(cookies);
+
+    if (user && ["/sign-in", "/sign-up"].includes(nextUrl.pathname)) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`);
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
